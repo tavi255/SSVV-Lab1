2cc3aee86e4e56ce6c932a170c88d85840425176
917_Octavian_Vărzaru <72062666+tavi255@users.noreply.github.com>
2023-03-24 18:42:31 +0200

    .

--- src/main/java/tests/TestStudent.java
+++ src/main/java/tests/TestStudent.java
@@ -1,12 +1,41 @@
 package tests;
 
+import domain.Student;
 import org.junit.jupiter.api.Test;
+import repository.NotaXMLRepo;
+import repository.StudentXMLRepo;
+import repository.TemaXMLRepo;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import view.UI;
+
+import static org.testng.AssertJUnit.assertTrue;
 
 public class TestStudent {
 
     @Test
     void addStudent()
     {
-        
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+        Student s=new Student("43","Luci",937,"luci@gmail.com");
+
+        assertTrue(service.)
+
+
     }
 }
