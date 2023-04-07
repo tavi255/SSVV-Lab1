c352ad42c16845ca4f4c24bea2474ddc14ca3db2
917_Octavian_Vărzaru <72062666+tavi255@users.noreply.github.com>
2023-04-07 18:51:00 +0300

    .

--- src/test/java/service/TestTema.java
+++ src/test/java/service/TestTema.java
@@ -1,6 +1,14 @@
 package service;
 
+import domain.Tema;
 import org.junit.Test;
+import repository.NotaXMLRepo;
+import repository.StudentXMLRepo;
+import repository.TemaXMLRepo;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.ValidationException;
 
 import static org.testng.AssertJUnit.*;
 
@@ -19,9 +27,26 @@ public class TestTema {
 
     }
 
-    @Test
+    @Test(expected=ValidationException.class)
     public void addTema3()
     {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+        Tema t1=new Tema("7","",3,1);
+
+
 
     }
 
