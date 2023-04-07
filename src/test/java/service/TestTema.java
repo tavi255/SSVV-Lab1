935dd3f89d7b36df81d993d4ecea611e374c7269
917_Octavian_Vărzaru <72062666+tavi255@users.noreply.github.com>
2023-04-07 18:52:21 +0300

    .

--- src/test/java/service/TestTema.java
+++ src/test/java/service/TestTema.java
@@ -46,13 +46,34 @@ public class TestTema {
 
         Tema t1=new Tema("7","",3,1);
 
+        service.addTema(t1);
+
 
 
     }
 
-    @Test
+    @Test(expected=ValidationException.class)
     public void addTema4()
     {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+        Tema t1=new Tema("7","dfsdfsd",-3,1);
+
+        service.addTema(t1);
+
+
 
     }
 
