5c2179277e4e55d062341b6aeceaeb891907634b
917_Octavian_Vărzaru <72062666+tavi255@users.noreply.github.com>
2023-04-07 18:44:55 +0300

    .

---
+++ src/test/java/service/TestTema.java
@@ -0,0 +1,41 @@
+package service;
+
+import org.junit.Test;
+
+import static org.testng.AssertJUnit.*;
+
+public class TestTema {
+
+
+
+    public void addTema1()
+    {
+
+    }
+
+    @Test
+    public void addTema2()
+    {
+
+    }
+
+    @Test
+    public void addTema3()
+    {
+
+    }
+
+    @Test
+    public void addTema4()
+    {
+
+    }
+
+
+    @Test
+    public void addTema5()
+    {
+
+    }
+
+}
